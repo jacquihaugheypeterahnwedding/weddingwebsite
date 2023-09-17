40df137d53c2632703b185c751a306c91f37e10e
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2023-09-17 10:37:17 -0400

    background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wedding-website';
 
   navLinks: any[];
   activeLinkIndex = -1; 
