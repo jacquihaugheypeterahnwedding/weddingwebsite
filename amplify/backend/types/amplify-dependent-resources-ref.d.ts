4af8ca0ce57651b2951dd2d23d8c7fde3dfecad2
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2023-09-17 09:12:55 -0400

    add amplify api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "weddingwebsite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
