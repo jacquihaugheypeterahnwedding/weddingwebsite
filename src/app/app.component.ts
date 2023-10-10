666c82d758e7ff0db8255005051ce41f295979f3
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2023-10-09 21:45:57 -0400

    add flower border

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CognitoService } from './cognito.service';
 
@@ -11,7 +11,8 @@ import { I18n } from 'aws-amplify';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
